9353b3eadc51d37de7900fee7e1eb563675af591
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-05-27 22:36:26 -0400

    EcmaScript 11

---
+++ src/es11/index.js
@@ -0,0 +1,35 @@
+//DYNAMIC IMPORT PERMITE IMPORTAR CODIGO DEPENDIENDE DE CUANDO SE NECESITE COMO ANGULAR
+const button = document.getElementById("btn");
+button.addEventListener('click',async function(){
+    const module = await import('./file.js');
+    module.hello();
+})
+
+//BIGN
+const aBigNumber =9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+//PROMISE ALLsettled
+const promise1 = new Promise((resolve, reject)=>reject('Reject'));
+const promise2 = new Promise((resolve, reject)=>resolve('Resolve'));
+const promise3 = new Promise((resolve, reject)=>resolve('Resolve 1'));
+//WAIT FOR ALL PROMISE
+Promise.allSettled([promise1,promise2,promise3]).then(response=>console.log(response));
+//IF ONE OF THIS IS REJECTED THIS WILL STOP
+Promise.all([promise1,promise2,promise3]).then(response=>console.log(response)).catch(error =>console.log('Error ',error));
+
+//OPERADOR NULL
+const foo =null??'default string';
+console.log(foo);
+
+//OPTIONAL CHAI
+const user = {};
+
+console.log(user?.profile?.email);
+if(user?.profile?.email){
+    console.log("email");
+}else{
+    console.log("fail");
+}
